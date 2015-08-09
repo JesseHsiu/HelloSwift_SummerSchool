0d36cef3e2847ce7812556a485de3234713c33fe
JesseHsiu <jesse801122@gmail.com>
2015-08-09 20:03:11 +0800

    add enum for Model

--- HelloSwift/UserDefineModel.swift
+++ HelloSwift/UserDefineModel.swift
@@ -8,6 +8,29 @@
 
 import Foundation
 
+enum TypeOfLevel : String
+{
+    case professor = "Professor"
+    case master = "Master Degree"
+    case shit = "Like a shit"
+    case notdefined = "Not defined"
+    
+    
+    static func randomLevel() -> TypeOfLevel {
+        let maxLevel: UInt32 = 4
+        let levelValue = Int(arc4random_uniform(maxLevel))
+        
+        switch levelValue {
+        case 0: return .professor
+        case 1: return .master
+        case 2: return .shit
+        case 3: return .notdefined
+        default: return .professor
+        }
+    }
+}
+
+
 class UserDefineModel : NSObject{
     
 }
