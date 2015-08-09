5a94dd4765061f44d4978bc31e620f2b5be6aeed
JesseHsiu <jesse801122@gmail.com>
2015-08-09 20:02:45 +0800

    add files

---
+++ HelloSwift/UserDefineButton.swift
@@ -0,0 +1,13 @@
+//
+//  UserDefineButton.swift
+//  HelloSwift
+//
+//  Created by 修敏傑 on 8/9/15.
+//  Copyright (c) 2015 NTU. All rights reserved.
+//
+
+import UIKit
+
+class UserDefineButton: UIButton {
+    
+}
---
+++ HelloSwift/UserDefineModel.swift
@@ -0,0 +1,13 @@
+//
+//  UserDefineModel.swift
+//  HelloSwift
+//
+//  Created by 修敏傑 on 8/9/15.
+//  Copyright (c) 2015 NTU. All rights reserved.
+//
+
+import Foundation
+
+class UserDefineModel : NSObject{
+    
+}
